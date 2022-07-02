555ae469e04a3a21137e59850a61138094a1cc40
Sebastian Cardona <cardonaloaizasebastian112@gmail.com>
2022-07-02 00:02:19 -0500

    pruebas unitarias

---
+++ test.js
@@ -0,0 +1,15 @@
+// Dependencias para pruebas
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+chai.should();
+chai.use(chaiHttp);
+
+// API
+const app = require('./index');
+
+describe("Prueba 1", () => {
+    it("Debe devolver un false", () => {
+        expect(false).to.equal(false);
+    })
+});
